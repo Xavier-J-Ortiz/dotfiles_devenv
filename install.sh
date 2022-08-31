b4315da64374c3288764a2e3175db523c48af543
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2022-08-30 21:06:51 -0400

    added check to make sure that config files exist before adding config to them.

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 sudo apt-get -y install vim-gtk3 vim-scripts powerline vim-pathogen tmux
-# .tmux.conf
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 
+BASH_CONFIG=~/.bashrc
+TMUX_CONFIG=~/.tmux.conf
+VIM_CONFIG=~/.vimrc
+SSH_AUTH_KEYS=~/.ssh/authorized_keys
+
+if [[ ! -f "$VIM_CONFIG" ]]; then
+  touch $VIM_CONFIG
+fi
 echo "call pathogen#infect()
 syntax on
 filetype plugin indent on
@@ -14,6 +21,9 @@ python3 del powerline_setup
 
 set laststatus=2" >> ~/.vimrc
 
+if [[ ! -f "$BASH_CONFIG" ]]; then
+  touch $BASH_CONFIG
+fi
 echo "if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
@@ -21,6 +31,9 @@ echo "if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
   source /usr/share/powerline/bindings/bash/powerline.sh
 fi" >> ~/.bashrc
 
+if [[ ! -f "$TMUX_CONFIG" ]]; then
+  touch $BASH_CONFIG
+fi
 echo 'set -g default-terminal "screen-256color"
 source "/usr/share/powerline/bindings/tmux/powerline.conf"
 
@@ -28,5 +41,9 @@ unbind C-b
 set -g prefix C-a
 bind C-a send-prefix' >> ~/.tmux.conf
 
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2L+WHRgzTECGb+dO4azOr7IrEIMI16b6nMU9wKxi/z xortiz@MainDesktop' >> .ssh/authorized_keys
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvul+3jmbRflJlO4ceNkLxsmI5kNuXTrUv6o1PlJh46 xortiz@Xaviers-MBP.fios-router.home' >> .ssh/authorized_keys
+if [[ ! -f "$SSH_AUTH_KEYS" ]]; then
+  touch $SSH_AUTH_KEYS
+fi
+
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2L+WHRgzTECGb+dO4azOr7IrEIMI16b6nMU9wKxi/z xortiz@MainDesktop' >> ~/.ssh/authorized_keys
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvul+3jmbRflJlO4ceNkLxsmI5kNuXTrUv6o1PlJh46 xortiz@Xaviers-MBP.fios-router.home' >> ~/.ssh/authorized_keys
