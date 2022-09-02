59a59ed2680f6574f0517f2ae33b2d624b3bb435
Xavier Ortiz <xavier.ortiz.ch@gmail.com>
2022-09-02 12:20:58 -0400

    modified private keys info

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
+
 sudo apt-get -y install vim-gtk3 vim-scripts vim-syntastic powerline vim-pathogen tmux
+
 if [[ ! -d ~/.vim/bundle/nerdtree ]]; then
   git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
 else
   cd ~/.vim/bundle/nerdtree && git pull origin master
 fi
+
+echo "$(cat ~/dotfiles/ssh/authorized_keys)" >> .ssh/authorized_keys
