5c72ad93dcb454b9be70e838949e57f5ae6a102a
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-26 17:33:06 -0500

    Modify `nvim-treesitter` config and plugin files to remove treesitter bootstrapping errors
    - Modifying the `nvim-treesitter` config and plugin files makes parsers load in a way that doesn't throw errors.
    - Modified the tokyonight color scheme. Want to try something different.
    - Small cleanup on `init.lua`.

---
+++ config/nvim/lua/config/nvim-treesitter.lua
@@ -0,0 +1,3 @@
+require'nvim-treesitter'.setup { install_dir = vim.fn.stdpath('data') .. '/site', }
+-- wait required so that there is no error when bootstrapping plugins on first run.
+require'nvim-treesitter'.install { 'lua', 'vimdoc', }:wait(300000)
