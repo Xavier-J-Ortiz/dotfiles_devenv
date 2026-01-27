bc8075f009fc1d143e1346412e3a15c26e611677
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-27 12:35:38 -0500

    Add formatting and linting plugins and tools
    - Add `conform.nvim` as the formatting plugin.
    - Add `nvim-lint` plugin as the linting plugin.
    - Add `mason-tool-installer.nvim` to manage installation of LSP, Linter, and Formatting software through integration with `Mason`.
    - Updated linters and formatters to install in `maston-tool-installer` config
      - Removed javascript formatters, as I currently don't have a need for them.
      - Added `ruff` as it's an all in one linter and formatter for python.
      - Added `ty` as it's a much needed typechecker for python.
    - Remove `config/mason-lspconfig.lua`, as LSP plugin installation on boostrap is done by `mason-tool-installer`.
    - Minor `config/vim-opt.lua` touch up.

---
+++ config/nvim/lua/config/mason-tool-installer.lua
@@ -0,0 +1,23 @@
+require("mason-tool-installer").setup({
+  -- Add linters and formatters only. LSPs are installed in the `mason-lspconfig` config.
+  dependencies = {
+    'mason-org/mason.nvim',
+    'mason-org/mason-lspconfig.nvim',
+  },
+  ensure_installed = {
+    -- linters
+    'golangci-lint', -- golang
+    'ruff', -- python linter & formatter
+    'luacheck', -- lua
+    -- formatters
+    'ty', -- python typechecker that complements `ruff`
+    'gofumpt', -- golang
+    'goimports', -- golang
+    'isort', -- python
+    'stylua', -- lua
+    -- LSPs formerly referenced in `mason-lspconfig`, to be used in in `lsp` config.
+    'gopls', -- golang
+    'basedpyright', -- python
+    'lua_ls', -- lua
+  },
+})
---
+++ config/nvim/lua/config/nvim-lint.lua
@@ -0,0 +1,5 @@
+require('lint').lint_by_ft = {
+  go = { 'golangci-lint', },
+  python = { 'ruff' },
+  lua = { 'luacheck' },
+}
