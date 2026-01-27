6ed91df74145f0017d7b505e09dbae03f3d366f2
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-26 23:45:18 -0500

    Add spelling settings in nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,2 @@
 require('config.lazy')
-vim.opt.number = true
--- Keymaps for diagnostics of errors/warnings/etc
--- All the below are done already natively.
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float) -- <C-w>d
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-vim.cmd.colorscheme "tokyonight-storm"
+require('config.vim-opts')
