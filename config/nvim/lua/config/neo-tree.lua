86193299e8626399f6504da4bed7fd02d95c4132
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-27 22:22:55 -0500

    Add which-key plugin
    - Modified the `<leader>e` neotree toggle

--- config/nvim/lua/config/neo-tree.lua
+++ config/nvim/lua/config/neo-tree.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>ne", "<Cmd>Neotree toggle<CR>")
 require("neo-tree").setup({
   close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
   popup_border_style = "NC", -- or "" to use 'winborder' on Neovim v0.11+
