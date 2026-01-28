a4337c20a8ca058d4e17f8079f00eb6898fc35bd
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-28 16:24:00 -0500

    Iterate over configuration
    - Fix loading of LSPs when server settings are empty.
      - Empty server settings should default.
    - Add packages to be installed via apt in `install.sh`.
    - Implement formatting via conform.nvim.
    - Add codespell formatter.
    - Fix vim ts/sts/sw/et settings.
    - Clean up ghostty and tmux configs.
    - Change neo-tree toggle keybind.
    - Clean formatting on fzf-lua and neo-tree text

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ sudo localectl set-locale LANG=en_US.UTF-8
 
 # sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt update
-sudo apt-get -y install vim-gtk3 python3-venv powerline tmux colordiff
+sudo apt-get -y install vim-gtk3 fzf python3-venv powerline tmux colordiff xclip wl-clipboard
+
+mkdir -p ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip && mkdir -p ~/.fonts && unzip ./Mononoki.zip -d ~/.fonts/ && rm Noto.zip && fc-cache -f -v
+
 
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -13,13 +17,10 @@ sudo apt-get -y install vim-gtk3 python3-venv powerline tmux colordiff
 # python3 -m pip install black==22.3.0 flake8==4.0.1 isort==5.10.1 mypy==0.950 pylint==2.13.8 vulture==2.3
 
 REPO_DIR=$(pwd)
-# cat ~/dotfiles/ssh/authorized_keys > ~/.ssh/authorized_keys
-ln -s $REPO_DIR/ssh/authorized_keys ~/.ssh/authorized_keys
-# cat ~/dotfiles/.vimrc > ~/.vimrc
+cat $REPO_DIR/ssh/authorized_keys | tee -a ~/.ssh/authorized_keys
 ln -s $REPO_DIR/vimrc ~/.vimrc
-# cat ~/dotfiles/tmux.conf > ~/.tmux.conf
 ln -s $REPO_DIR/tmux.conf ~/.tmux.conf
-# cat ~/dotfiles/ > ~/.bashrc
 ln -s $REPO_DIR/bashrc ~/.bashrc
 ln -sn $REPO_DIR/config/nvim ~/.config/nvim
 ln -sn $REPO_DIR/config/ghostty ~/.config/ghostty
+ln -sn $REPO_DIR/config/alacritty ~/.config/alacritty
