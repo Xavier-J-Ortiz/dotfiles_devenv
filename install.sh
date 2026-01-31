503f20c0b46d40cd5c55d23ea62e6953474e6cfc
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-31 16:10:57 -0500

    Update install.sh - Copy ssh keys in .ssh/authorized_keys, unless they already exist. - Add and install latest nvim package from `neovim/neovim-releases`. - Minor update to `.gitignore`.

--- install.sh
+++ install.sh
@@ -5,19 +5,36 @@ sudo localectl set-locale LANG=en_US.UTF-8
 
 # sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt update
-sudo apt-get -y install vim-gtk3 fzf python3-venv powerline tmux colordiff xclip wl-clipboard
+sudo apt-get -y install git vim-gtk3 fzf python3-venv powerline tmux colordiff xclip wl-clipboard
 
 mkdir -p ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip && mkdir -p ~/.fonts && unzip ./Mononoki.zip -d ~/.fonts/ && rm Noto.zip && fc-cache -f -v
 
+# install nvim from neovim/neovim-releases if not present
+if ! type nvim > /dev/null 2>&1; then
+    sudo apt-get autoremove
+    echo "neovim is not present. Installing \`latest\` from the deb package from the github repo neovim/neovim-releases."
+    latest=$(git ls-remote --tags https://github.com/neovim/neovim-releases | grep -Eo 'refs/tags/v([0-9]+(\.)?)+' | sort -r | head -n1 | cut -f3 -d'/')
+    wget https://github.com/neovim/neovim-releases/releases/download/$latest/nvim-linux-x86_64.deb
+    sudo dpkg -i nvim-linux-x86_64.deb
+    rm nvim-linux-x86_64.deb
+fi
 
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.zip && mkdir -p ~/.fonts && unzip -o ./Mononoki.zip -d ~/.fonts/ && rm ./Mononoki.zip && fc-cache -f -v
 
-# get some linters from pip
-# python3 -m pip install black==22.3.0 flake8==4.0.1 isort==5.10.1 mypy==0.950 pylint==2.13.8 vulture==2.3
-
+# Add authorized_keys if the pub key fingerprint doesn't already exist in file.
 REPO_DIR=$(pwd)
-cat $REPO_DIR/ssh/authorized_keys | tee -a ~/.ssh/authorized_keys
+mkdir -p ~/.ssh
+while IFS= read -r key; do
+    if [[ -n "$key" ]]; then
+        key_fingerprint=$(echo "$key" | ssh-keygen -lf - 2>/dev/null | awk '{print $2}')
+        if [[ -n "$key_fingerprint" ]] && ! grep -q "$key_fingerprint" <(ssh-keygen -lf ~/.ssh/authorized_keys 2>/dev/null); then
+            echo "$key" >> ~/.ssh/authorized_keys
+        fi
+    fi
+done < "$REPO_DIR/ssh/authorized_keys"
+
 ln -s $REPO_DIR/vimrc ~/.vimrc
 ln -s $REPO_DIR/tmux.conf ~/.tmux.conf
 ln -s $REPO_DIR/bashrc ~/.bashrc
