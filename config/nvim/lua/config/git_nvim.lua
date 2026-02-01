6cd53c34c2e6f2263e1647aa5f7ad03f180561bc
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-31 22:35:49 -0500

    Add git.nvim and gitsigns plugin, fix keybinding
    - Fix opencode <C-.> keybind

---
+++ config/nvim/lua/config/git_nvim.lua
@@ -0,0 +1,50 @@
+require("git").setup({
+	default_mappings = true, -- NOTE: `quit_blame` and `blame_commit` are still merged to the keymaps even if `default_mappings = false`
+
+	keymaps = {
+		-- Open blame window
+		blame = "<Leader>gb",
+		-- Close blame window
+		quit_blame = "q",
+		-- Open blame commit
+		blame_commit = "<CR>",
+		-- Quit blame commit
+		quit_blame_commit = "q",
+		-- Open file/folder in git repository
+		browse = "<Leader>go",
+		-- Open pull request of the current branch
+		open_pull_request = "<Leader>gp",
+		-- Create a pull request with the target branch is set in the `target_branch` option
+		create_pull_request = "<Leader>gn",
+		-- Opens a new diff that compares against the current index
+		diff = "<Leader>gd",
+		-- Close git diff
+		diff_close = "<Leader>gD",
+		-- Revert to the specific commit
+		revert = "<Leader>gr",
+		-- Revert the current file to the specific commit
+		revert_file = "<Leader>gR",
+	},
+	-- Default target branch when creating a pull request
+	-- Can be either:
+	--   - A string (e.g., "master", "main", "develop")
+	--   - A function that returns a string (e.g., function() return "main" end)
+	-- By default, it dynamically detects the default branch from git remote
+	target_branch = function()
+		local handle = io.popen("git remote show origin 2>/dev/null | grep 'HEAD branch' | cut -d' ' -f5")
+		if handle then
+			local result = handle:read("*a")
+			handle:close()
+			local branch = result:match("^%s*(.-)%s*$")
+			if branch and branch ~= "" then
+				return branch
+			end
+		end
+
+		return "master"
+	end,
+	-- Private gitlab hosts, if you use a private gitlab, put your private gitlab host here
+	-- private_gitlabs = { "https://xxx.git.com" },
+	-- Enable winbar in all windows created by this plugin
+	winbar = false,
+})
