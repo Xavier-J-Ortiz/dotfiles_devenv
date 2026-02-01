9d1af182fbd1be2fc064e3aa3f316ace433607ad
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-01-31 23:50:23 -0500

    Add check if user is xortiz before adding a pubkey to `.ssh/authorized_keys`

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
 
 sudo localectl set-locale LANG=en_US.UTF-8
 
-# sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt update
 sudo apt-get -y install git luarocks vim-gtk3 fzf python3-venv powerline tmux colordiff xclip wl-clipboard
 
@@ -22,15 +21,19 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Mononoki.z
 
 # Add authorized_keys if the pub key fingerprint doesn't already exist in file.
 REPO_DIR=$(pwd)
-mkdir -p ~/.ssh
-while IFS= read -r key; do
-    if [[ -n "$key" ]]; then
-        key_fingerprint=$(echo "$key" | ssh-keygen -lf - 2>/dev/null | awk '{print $2}')
-        if [[ -n "$key_fingerprint" ]] && ! grep -q "$key_fingerprint" <(ssh-keygen -lf ~/.ssh/authorized_keys 2>/dev/null); then
-            echo "$key" >> ~/.ssh/authorized_keys
+
+# Unless you're me, you probably don't want to give me ssh access to your system.
+if [[ $USER == 'xortiz' ]]; then
+    mkdir -p ~/.ssh
+    while IFS= read -r key; do
+        if [[ -n "$key" ]]; then
+            key_fingerprint=$(echo "$key" | ssh-keygen -lf - 2>/dev/null | awk '{print $2}')
+            if [[ -n "$key_fingerprint" ]] && ! grep -q "$key_fingerprint" <(ssh-keygen -lf ~/.ssh/authorized_keys 2>/dev/null); then
+                echo "$key" >> ~/.ssh/authorized_keys
+            fi
         fi
-    fi
-done < "$REPO_DIR/ssh/authorized_keys"
+    done < "$REPO_DIR/ssh/authorized_keys"
+fi
 
 ln -s $REPO_DIR/vimrc ~/.vimrc
 ln -s $REPO_DIR/tmux.conf ~/.tmux.conf
