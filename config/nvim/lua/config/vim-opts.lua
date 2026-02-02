307641d789baeed85c2004245a994dea0492ed66
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-02-02 17:47:24 -0500

    Remove unused theme, remove mouse interaction

--- config/nvim/lua/config/vim-opts.lua
+++ config/nvim/lua/config/vim-opts.lua
@@ -10,3 +10,5 @@ vim.opt.tabstop = 4 -- Displayed width of a tab
 vim.opt.shiftwidth = 4 -- How much is moved on >>
 vim.opt.softtabstop = 4 -- Inserts 4 spaces if Tab is set, and expandtab is true
 vim.opt.expandtab = true -- On enable, inserts spaces instead of tab.
+-- disable mouse support
+vim.opt.mouse = ""
--- config/nvim/lua/plugins/tokyonight.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"folke/tokyonight.nvim",
-	config = function()
-		require("config.tokyonight")
-	end,
-}
