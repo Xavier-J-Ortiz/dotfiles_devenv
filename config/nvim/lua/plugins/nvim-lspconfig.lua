779c6f7debdf52a77d5277f26464779231ef6758
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-02-16 11:54:58 -0500

    Continuing to modify config files - Add ty and remark_ls as LSPs. - Add linter, formatter, and LSP for markdown - Modify `install.sh`.   - Add build essential packages to apt install.   - Add install of pyenv. - Change filename from `lsp.lua` to `nvim-lspconfig.lua` - Remove markdownlint-cli2 from conform and nvim-lint - Remove ruff from nvim-lint. - Remove opinionated vale packages for a good default experience on non-project markdown, text, or RST files.

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		-- TODO: after evaluating what stays in `config.lsp`, maybe also point to a `config.nvim-lspconfig`
-		require("config.lsp")
+		require("config.nvim-lspconfig")
 	end,
 }
