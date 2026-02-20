e069b1b29943e123595730c582bb862474002a48
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-02-19 22:13:05 -0500

    Additional treesitter updates

--- config/nvim/lua/config/nvim-treesitter.lua
+++ config/nvim/lua/config/nvim-treesitter.lua
@@ -1,3 +1,9 @@
-require("nvim-treesitter").setup({ install_dir = vim.fn.stdpath("data") .. "/site" })
+require("nvim-treesitter").setup({
+	install_dir = vim.fn.stdpath("data") .. "/site",
+	ensure_installed = { "lua", "vimdoc", "markdown", "python", "go" },
+	highlight = {
+		enable = true,
+	},
+})
 -- wait required so that there is no error when bootstrapping plugins on first run.
 require("nvim-treesitter").install({ "lua", "vimdoc", "markdown", "python", "go" }):wait(300000)
