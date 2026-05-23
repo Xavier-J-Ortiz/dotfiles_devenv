292e250cec00a43584847cff24a93bd3c9d1e068
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-05-22 22:25:57 -0400

    adding wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font('Mononoki Nerd Font Mono'),
+  font_size = 11.0,
+
+  color_scheme = 'Dracula+',
+
+  window_close_confirmation = 'NeverPrompt',
+  window_decorations = 'RESIZE',
+  adjust_window_size_when_changing_font_size = false,
+
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+
+  -- Enable scrollback
+  scrollback_lines = 10000,
+}
