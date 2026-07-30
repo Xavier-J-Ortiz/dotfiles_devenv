a118c9abb560a6187c30f5a072b1f46b886ea8bf
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-07-29 22:18:59 -0400

    Issue #54: Update opencode.nvim version and settings
    - Update defaults for opencode.lua and snacks_nvim.lua
    - Add kitty and supporting config to allow <C-.> capability of opencode.nvim
    - Allow server location to update on <C-.> press

--- config/nvim/lua/config/blink.lua
+++ config/nvim/lua/config/blink.lua
@@ -26,8 +26,9 @@ return {
 	-- elsewhere in your config, without redefining it, due to `opts_extend`
 	sources = {
 		default = { "lsp", "path", "snippets", "buffer" },
+		-- Display buffer completions (if included above) when no LSP completions are available
+		providers = { lsp = { fallbacks = {} } },
 	},
-
 	-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
 	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
--- config/nvim/lua/plugins/snacks_nvim.lua
+++ config/nvim/lua/plugins/snacks_nvim.lua
@@ -1,22 +1,33 @@
 return {
 	"folke/snacks.nvim",
+	---@type snacks.Config
 	opts = {
-		input = {},
-		terminal = {
-			enabled = true,
+		input = {
+			enabled = true, -- Enhances Ask
 		},
 		picker = {
-			actions = {
-				opencode_send = function(...)
-					return require("opencode").snacks_picker_send(...)
-				end,
-			},
+			enabled = true, -- Enhances Select
 			win = {
 				input = {
 					keys = {
-						["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+						["<a-o>"] = { "opencode_send", mode = { "n", "i" } },
 					},
 				},
+			},
+			actions = {
+				opencode_send = function(picker) ---@param picker snacks.Picker
+					local items = vim.tbl_map(function(item) ---@param item snacks.picker.Item
+						return item.file
+								and require("opencode").format({
+									path = item.file,
+									from = item.pos,
+									to = item.end_pos,
+								})
+							or item.text
+					end, picker:selected({ fallback = true }))
+
+					require("opencode").prompt(table.concat(items, ", ") .. " ")
+				end,
 			},
 		},
 	},
