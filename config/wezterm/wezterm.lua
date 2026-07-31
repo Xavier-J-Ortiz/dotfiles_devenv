a5c7b590098497cb2c67555798f0c8a6ae309c58
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-07-30 21:47:25 -0400

    Issue #54: tmux and wezterm config
    - Simplify extended keys setup and drop version check.
    - Update wezterm config to work in wayland.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,19 +1,20 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font('Mononoki Nerd Font Mono'),
-  font_size = 11.0,
+	default_prog = { "bash", "-i" },
+	font = wezterm.font("Mononoki Nerd Font Mono"),
+	font_size = 11.0,
 
-  color_scheme = 'Dracula+',
+	color_scheme = "Dracula+",
 
-  window_close_confirmation = 'NeverPrompt',
-  window_decorations = 'RESIZE',
-  adjust_window_size_when_changing_font_size = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "TITLE | RESIZE",
+	adjust_window_size_when_changing_font_size = false,
 
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
 
-  -- Enable scrollback
-  scrollback_lines = 10000,
+	-- Enable scrollback
+	scrollback_lines = 10000,
 }
