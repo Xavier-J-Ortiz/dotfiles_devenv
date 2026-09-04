d586fb654a40726ee36bb93edaac7858c539d36a
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-09-04 09:23:44 -0400

    lualine and nvim-{lint,lspconfig,treesitter} consolidations

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,16 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
 	config = function()
-		require("config.nvim-treesitter")
+		require("nvim-treesitter")
+			.install({
+				"lua",
+				"vimdoc",
+				"markdown",
+				"python",
+				"go",
+				"bash",
+			})
+			:wait(300000)
 	end,
 	build = ":TSUpdate",
 }
