e68a9a9e1abd2c1edaf7b72981147cc9f0b05c5f
Xavier J. Ortiz <xavier.ortiz.ch@gmail.com>
2026-09-07 09:43:29 -0400

    Consolidating and refactoring lazy.nvim and vim-opts files into init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,52 @@
-require("config.lazy")
-require("config.vim-opts")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- Editor options
+vim.opt.number = true
+-- spelling
+local spell_dir = vim.fn.stdpath("config") .. "/spell"
+vim.fn.mkdir(spell_dir, "p")
+vim.opt.spellfile = vim.fn.expand("~/.config/nvim/spell/en.utf-8.add")
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+
+vim.opt.tabstop = 4 -- Displayed width of a tab
+vim.opt.shiftwidth = 4 -- How much is moved on >>
+vim.opt.softtabstop = 4 -- Inserts 4 spaces if Tab is set, and expandtab is true
+vim.opt.expandtab = true -- On enable, inserts spaces instead of tab.
+-- disable mouse support
+vim.opt.mouse = ""
+vim.opt.termguicolors = true
